8107fc28582bcd7a074e829572bbb045242e432c
Matthew Baggett <matthew@baggett.me>
2015-04-27 17:33:55 +0100

    Added another test. 32% coverage now.

--- tests/LanguageTest.php
+++ tests/LanguageTest.php
@@ -1,20 +1,26 @@
 <?php
+namespace Thru\Translation\Test;
 use Thru\Translation\Translation;
 use Thru\Translation\Models\Language;
 
-class LanguageTest extends PHPUnit_Framework_TestCase {
+class LanguageTest extends BaseTest {
 
   /** @var $tr Translation */
   private $tr;
 
   public function setUp(){
+    parent::setUp();
     $this->tr = Translation::getInstance();
     $this->tr->configure_original_language('en');
     $this->tr->configure_target_language('fr');
-    return;
   }
 
   public function testTranslateEnToFr(){
-    $this->assertEquals("bonjour", strtolower($this->tr->translate("Hello")));
+    $this->assertEquals("Bonjour", $this->tr->translate("Hello"));
+  }
+
+  public function testTranslateEnToFrParameters(){
+    $name = $this->faker->firstName();
+    $this->assertEquals("Bonjour {$name}", $this->tr->translate("Hello :name", [':name' => $name]));
   }
 }
